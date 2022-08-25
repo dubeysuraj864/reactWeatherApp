7f3b3407ef862ba82fc3f9fdc1a6e5ac479a7f7b
Suraj Dubey <dubeysuraj864@gmail.com>
2022-08-25 21:23:00 +0530

    3

--- src/App.js
+++ src/App.js
@@ -3,54 +3,68 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [city, setCity] = useState({});
-  const [search, setSearch] = useState("Delhi");
 
-  useEffect(() => {
-    const getData = async () => {
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&limit=0&lang=en&appid=dbbedde9ee53a9bcac9ac6dc3d629413`
-      );
+  const[lat, setLat] = useState([]);
+  const[lon, setLon] = useState([]);
+  const[data, setData] = useState([]);
 
-      const data = await response.json();
-      setCity(data);
-      console.log(data);
-    };
-    getData();
-  }, [setSearch]);
+  useEffect(()=>{
+    const fetchData = async () => {
+
+      navigator.geolocation.getCurrentPosition(
+        function(position){
+          setLat(position.coords.latitude);
+          setLon(position.coords.longitude);
+        });
+
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&limit=0&lang=en&appid=dbbedde9ee53a9bcac9ac6dc3d629413`);
+
+        const data = await response.json();
+        setData(data);
+       
+    }
+
+
+    fetchData();
+
+  },[lat, lon])
+   console.log("lat:", lat);
+    console.log("lon:", lon);
+    console.log(data)
+
 
   return (
     <div className="App">
       <h1>Weather App</h1>
 
       <div className="container">
-        <input
+        {/* <input
           type="text"
-          value={search}
+          defaultValue="Delhi"
           onChange={(event) => {
-            setSearch(event.target.value);
+            // setSearch(event.target.value);
           }}
-        />
+        /> */}
       </div>
+    
 
       <div className="weather-img">
         <img className="cloud-1" src={cloud} alt="Cloud" />
         <img className="cloud-2" src={cloud} alt="Cloud" />
       </div>
-      <div className="location">{search}</div>
+      <div className="location">{data.name}</div>
       <div className="temp">
-        {city.main.temp} °Celsius <br />{" "}
-        {(city.main.temp * (9 / 5) + 32).toFixed(2)} °Fahrenheit
+       0 °Celsius <br />{" "}
+        {( 0 * (9 / 5) + 32).toFixed(2)} °Fahrenheit
       </div>
-      {!city ? (
-        <p>No data found</p>
-      ) : (
+      {
+        (!data) ? (<p>No data found</p>): (
         <div className="details">
-       
-          <div className="weather-detail">Weather: {city.weather[0].main}</div>
-        <div className="wind">Wind: {city.wind.speed}  mph</div>
+          {/* <div className="wind">Wind: {data.main.temp} mph</div> */}
         </div>
-      )}
+        )
+      }
+    
     </div>
   );
 }
